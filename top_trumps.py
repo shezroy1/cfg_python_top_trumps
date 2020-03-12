db817aa903593eef299bd3baff1de8181f511bb9
psyssr <sherinroy1@outlook.com>
2020-03-12 18:57:59 +0000

    moved writing to file into a function, changed file path of tournament.txt to appear in current directory

--- top_trumps.py
+++ top_trumps.py
@@ -1,5 +1,27 @@
 # #1. generate random id
 import random
+import requests
+from pprint import pprint
+
+
+def store_result_to_file(winner_of_round, chosen_stat_of_round, stat_comp_of_round, stat_user_of_round):
+    # 5. record result to the file
+    with open('./top_trumps/tournament.txt', 'a') as text_write:
+        add = 'winner: {}'.format(winner_of_round) + ' ; comparing: {}'.format(
+            chosen_stat_of_round) + ' ; score: {}'.format(
+            stat_user_of_round[chosen_stat_of_round]) + ' : {} \n'.format(stat_comp_of_round[chosen_stat_of_round])
+        text_write.write(add)
+        text_write.close()
+
+
+def call_API_and_save_to_dict(number, dict_general):
+    response = requests.get(url.format(number))
+    json = response.json()
+    dict_general["name"] = json["name"]
+    dict_general["id"] = json["id"]
+    dict_general["height"] = json["height"]
+    dict_general["weight"] = json["weight"]
+
 
 rand_id_user = random.randint(1, 151)
 rand_id_comp = random.randint(1, 151)
@@ -18,21 +40,8 @@ stat_comp = {
     'height': '',
     'weight': '',
 }
-# #3. request from API
-import requests
-from pprint import pprint
 
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'
-
-
-def call_API_and_save_to_dict(number, dict_general):
-    response = requests.get(url.format(number))
-    json = response.json()
-    dict_general["name"] = json["name"]
-    dict_general["id"] = json["id"]
-    dict_general["height"] = json["height"]
-    dict_general["weight"] = json["weight"]
-
 
 call_API_and_save_to_dict(rand_id_user, stat_user)
 call_API_and_save_to_dict(rand_id_comp, stat_comp)
@@ -42,7 +51,7 @@ pprint(stat_comp)
 
 # 4. choose stats to compare
 winner = ''
-chosen_stat = input('Which stats do you want to compare? height/weight')
+chosen_stat = input('Which stats do you want to compare? height/weight\n')
 if chosen_stat == 'weight':
     if stat_user['weight'] >= stat_comp['weight']:
         print('You win!')
@@ -58,9 +67,4 @@ if chosen_stat == 'height':
         print('Computer wins 😞')
         winner = "computer"
 
-# 5. record result to the file
-with open('tournament.txt', 'a') as text_write:
-    add = 'winner: {}'.format(winner) + ' ; comparing: {}'.format(chosen_stat) + ' ; score: {}'.format(
-        stat_user[chosen_stat]) + ' : {} \n'.format(stat_comp[chosen_stat])
-    text_write.write(add)
-    text_write.close()
+store_result_to_file(winner, chosen_stat, stat_comp, stat_user)
